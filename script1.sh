be1d6d02ba113e5945ace84757e85f54639dd0d6
Danila Volodin <voldanila@gmail.com>
2022-11-11 19:17:27 +0300

    11

--- script1.sh
+++ script1.sh
@@ -3,4 +3,7 @@ for i in {1..5}; do base64 /dev/urandom | head -c 100000 > /home/danila/file$i.t
 for i in {1..5}; do rsync -avz /home/danila/file$i.txt root@84.201.152.71:/tmp; done
 ssh root@84.201.152.71 "find /tmp -mtime +7 -name 'file*.txt' -delete"
 
+111
 
+11
+1111
