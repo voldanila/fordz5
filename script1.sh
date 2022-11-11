2753ac7e5f59df822e8e662c648c8d8116c2358c
Danila <voldanila@gmail.com>
2022-11-11 15:50:29 +0000

    commit nomer dva

---
+++ script1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+for i in {1..5}; do base64 /dev/urandom | head -c 100000 > /home/danila/file$i.txt; done 
+for i in {1..5}; do rsync -avz /home/danila/file$i.txt root@84.201.152.71:/tmp; done
+ssh root@84.201.152.71 "find /tmp -mtime +7 -name 'file*.txt' -delete"
+
+
